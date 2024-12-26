67519c72850c176de9a8f16ab96046b8890e1181
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2024-12-26 12:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  const currentSubArray = [];
+  let currentSum = 0;
+
+  arr.forEach((num) => {
+    if (currentSum + num > n) {
+      result.push([...currentSubArray]);
+      currentSubArray.length = 0;
+      currentSum = 0;
+    }
+
+    currentSubArray.push(num);
+    currentSum += num;
+  });
+
+  if (currentSubArray.length > 0) {
+    result.push(currentSubArray);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
+const n = Number(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
